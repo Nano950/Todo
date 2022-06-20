db8639b1b8039a6bbf6febd8fad6e5a9337cc7ae
Dakota Hanks <dakotahanks1020@gmail.com>
2022-06-20 16:38:46 -0400

    sorted code blocks

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 let todoItems = [];
 
+function renderTodo(todo) {
+  const list = document.getElementById('list-item-container');
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+
+  if (todo.deleted) {
+    item.remove();
+    return
+    }
+
+  node.setAttribute('class', `list-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+  <input id="${todo.id}" type="checkbox" class="check"/>
+  <label for="${todo.id}" class="tick js-tick">
+  <span>${todo.text}</span>
+  </label>
+  <span class="close"><img src = "./images/icon-cross.svg"></span>
+  `;
+
+  list.append(node);
+}
+
 function addTodo(text) {
-    const todo = {
-      text,
-      checked: false,
-      id: Date.now(),
-    };
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
   
     todoItems.push(todo);
     renderTodo(todo);
 }
-
-
-const form = document.forms[0]; //might cause an error
-form.addEventListener('submit', event => {
-    event.preventDefault(event);
-    const input = document.getElementById('todo-input');
-    const text = input.value.trim();
-    if (text !== '') {
-        addTodo(text);
-        input.value = '';
-        input.focus();
-  }
-});
-
-
-
-const list = document.getElementById('list-item-container');
-list.addEventListener('click', event => {
-  if (event.target.classList.contains('js-tick')) {
-    const itemKey = event.target.parentElement.dataset.key;
-    toggleDone(itemKey);
-  }
-
-  if (event.target.classList.contains('js-delete-todo')) {
-    const itemKey = event.target.parentElement.dataset.key;
-    deleteTodo(itemKey);
-  }
-
-});
 
 function toggleDone(key) { //should NOT cross out text in todo item (it doesnt) ;-;
   const index = todoItems.findIndex(item => item.id === Number(key));
@@ -56,27 +50,28 @@ function deleteTodo(key) {
   renderTodo(todo);
 }
 
-function renderTodo(todo) {
-    const list = document.getElementById('list-item-container');
-    const isChecked = todo.checked ? 'done': '';
-    const node = document.createElement("li");
+const form = document.forms[0]; //might cause an error
+form.addEventListener('submit', event => {
+  event.preventDefault(event);
+  const input = document.getElementById('todo-input');
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
 
-    if (todo.deleted) {
-      item.remove();
-      return
-    }
+const list = document.getElementById('list-item-container');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
 
-    node.setAttribute('class', `list-item ${isChecked}`);
-    node.setAttribute('data-key', todo.id);
-    node.innerHTML = `
-    <input id="${todo.id}" type="checkbox" class="check"/>
-    <label for="${todo.id}" class="tick js-tick">
-    <span>${todo.text}</span>
-    </label>
-    <span class="close"><img src = "./images/icon-cross.svg"></span>
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
 
-  `;
-
-  list.append(node);
-}
-
+});
